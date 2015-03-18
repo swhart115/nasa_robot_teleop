4c390dbea842cc7244cb16c8b399f97aa4e5a18f
Stephen Hart <swhart@traclabs.com>
2015-03-18 15:35:30 -0500

    fixed depenedency issue

--- nodes/interactive_control.py
+++ nodes/interactive_control.py
@@ -32,8 +32,6 @@ from nasa_robot_teleop.tolerances import *
 
 # path planner instances
 from nasa_robot_teleop.path_planner import *
-from nasa_robot_teleop.planners.moveit_path_planner import *
-from nasa_robot_teleop.planners.atlas_path_planner import *
 from nasa_robot_teleop.group_config_parser import *
 from nasa_robot_teleop.group_config import *
 
@@ -126,8 +124,10 @@ class InteractiveControl:
        
         # planner instantiatation
         if planner_type == "moveit" :
+            from nasa_robot_teleop.planners.moveit_path_planner import *
             self.path_planner = MoveItPathPlanner(self.robot_name, self.planner_config_file)
         elif planner_type == "atlas" :
+            from nasa_robot_teleop.planners.atlas_path_planner import *
             self.path_planner = AtlasPathPlanner(self.robot_name, self.planner_config_file)
         else :
             rospy.logerr("InteractiveControl() unrecognized planner type!!")
